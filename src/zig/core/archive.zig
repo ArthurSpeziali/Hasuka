badce42beb1d8967d0a57923e9a0bde1c4077aba
ArthurSpeziali <arthur.speziali2022@gmail.com>
2026-07-24 18:41:12 -0300

    Small correction in Wellcome message

--- src/zig/core/archive.zig
+++ src/zig/core/archive.zig
@@ -8,10 +8,10 @@ pub const isCOM_OUTPUT: bool = false;
 
 // Public Constants in the Kernel
 pub const WELCOME_STR: []const u8 =
-    \\=========================================-----------------
+    \\=========================================----------------
     \\|  Welcome! You're in Hasuka System.    | / Version: \ |
     \\|       The Sky is your Dreams!         | \ 0.1.0    / |
-    \\=========================================-----------------
+    \\=========================================----------------
     \\             
     \\
 ;
